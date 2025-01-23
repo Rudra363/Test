4e409f3bec3cf5017f9a000e90696ec8be1c7fc8
Rudra363 <102245619+Rudra363@users.noreply.github.com>
2025-01-23 18:11:49 +0000

    Update App.java
    testing git pull

--- trial/src/main/java/uk/ac/nulondon/App.java
+++ trial/src/main/java/uk/ac/nulondon/App.java
@@ -8,5 +8,7 @@ public final class App {
         System.out.println("Hello World!");
         System.out.println("trial");
         System.out.println("Sanjeet poo poo");
+        System.out.println("pull");
+
     }
 }
