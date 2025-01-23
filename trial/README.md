b1956b059d71750d8364a3966faf59dc41ad3f10
Rudra363 <Rudra1hp@gmail.com>
2025-01-23 14:39:59 +0000

    Added a short message

---
+++ trial/README.md
@@ -0,0 +1,7 @@
+# uk.ac.nulondon:trial
+
+Write your project description here...
+
+Java version 21.
+
+Generated at 2025-01-23 14:31:45 by [homework-quickstart](https://github.com/atp-mipt/homework-quickstart) v.2.0
---
+++ trial/src/main/java/uk/ac/nulondon/App.java
@@ -0,0 +1,11 @@
+package uk.ac.nulondon;
+
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("trial");
+    }
+}
