1bb5d8d1fde5f536266af712f2b9a95c2e40422b
Rudra363 <Rudra1hp@gmail.com>
2025-01-23 18:36:19 +0000

    as

--- trial/src/main/java/uk/ac/nulondon/App.java
+++ trial/src/main/java/uk/ac/nulondon/App.java
@@ -6,10 +6,17 @@ public final class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello Wo");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello fsfsfs");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
     }
 
     public void counter (int x, int y) {
-        System.out.println("x=" + x + ", y=" + y);
+        System.out.println("x=" + x + ", y=" + y + "d" + "s");
     }
 
 }
